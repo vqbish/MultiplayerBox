a2ee93a5c4131df76d1e5f1c93e225aee7152c48
vqbish <clitbo228@gmail.com>
2026-05-10 16:30:07 +0300

    Update Init.lua

--- Scripts/Init.lua
+++ Scripts/Init.lua
@@ -1,17 +1,3 @@
-Debug.Log(File.GetModName() .. " Init. Trying to load other modules...")
-local found = FindFilesWithName("Includes.lua", File.GetModName() .. "/Scripts")
-if #found <= 0 then
-    Debug.Log("Critical error in mod loading: includes not found")
-    return
-end
-Includes = File.DoFile(found[#found])
-Debug.Log("Includes.lua loaded.")
-
-function Update()
-    TickManager.Update()
-    NetworkManager.Update()
-end
-
 
 function FindFilesWithName(targetName, relPath)
     local foundFiles = {}
@@ -42,3 +28,20 @@ function FindFilesWithName(targetName, relPath)
     searchInDirectory(relPath)
     return foundFiles
 end
+Debug.Log(File.GetModName() .. " Init. Trying to load other modules...")
+local found = FindFilesWithName("Includes.lua", File.GetModName() .. "/Scripts")
+if #found <= 0 then
+    Debug.Log("Critical error in mod loading: includes not found")
+end
+Includes = File.DoFile(found[#found])
+Debug.Log("Includes.lua loaded.")
+
+function Update()
+    TickManager.Update()
+    NetworkManager.Update()
+end
+
+function OnGet(args, time)
+    Logger.Log("Args: " .. json.serialize(args) .. " Time: " .. basicModule.tostring(time))
+end
+NetworkManager.AddOnGetListener(OnGet)
